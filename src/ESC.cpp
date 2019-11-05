a72853fd860d999d2055000b587f4ffe470df651
Walt Sorensen <photodude@users.noreply.github.com>
2019-11-05 09:55:28 -0700

    Add get/set for Calibration delay and Stop Pulse values
    Calibration delay and the Stop Pulse value should be refactored with Getter and Setter options
    Currently, these values are library constants ESC_CAL_DELAY and ESC_STOP_PULSE but are something that the user may need to change for their specific ESC.

--- src/ESC.cpp
+++ src/ESC.cpp
@@ -1,14 +1,15 @@
-/* Electronic Speed Controller (ESC) - Library */
-
 /*
-	
-*/
+ * Electronic Speed Controller (ESC) - Library
+ *
+ *
+ */
 
 #include "ESC.h"
 
 // < Constructor >
-/* Sets the proper pin to output.
-*/
+/*
+ * Sets the proper pin to output.
+ */
 ESC::ESC(byte ESC_pin, int outputMin, int outputMax, int armVal)
 {
 	oPin = ESC_pin;
@@ -23,9 +24,10 @@ ESC::~ESC()
 	// Nothing to destruct
 }
 
-// calib
-/* Calibrate the maximum and minimum PWM signal the ESC is expecting
-*/
+/*
+ * Calibrate the maximum and minimum PWM signal the ESC is expecting
+ * depends on the outputMin, outputMax values from the constructor
+ */
 void ESC::calib(void)
 {
 	myESC.attach(oPin);  			// attaches the ESC on pin oPin to the ESC object
@@ -36,17 +38,67 @@ void ESC::calib(void)
 	arm();
 }
 
+/*
+ * Sent a signal to Arm the ESC
+ * depends on the Arming value from the constructor
+ */
 void ESC::arm(void)
 {
 	myESC.attach(oPin);  			// attaches the ESC on pin oPin to the ESC object
 	myESC.writeMicroseconds (oArm);
 }
+
+/*
+ * Sent a signal to stop the ESC
+ * depends on the ESC stop pulse value
+ */
 void ESC::stop(void)
 {
 	myESC.writeMicroseconds (ESC_STOP_PULSE);
 }
+
+/*
+ * Sent a signal to set the ESC speed
+ * depends on the calibration minimum and maximum values
+ */
 void ESC::speed(int outputESC)
 {
 	oESC = constrain(outputESC, oMin, oMax);
 	myESC.writeMicroseconds(oESC);
+}
+
+/*
+ * Get the current calibration delay in miliseconds
+ *
+ */
+uint32_t getCalibrationDelay(void)
+{
+	return calibrationDelay;
+}
+
+/*
+ * Set the current calibration delay in miliseconds
+ *
+ */
+void setCalibrationDelay(uint32_t calibration_delay)
+{
+	calibrationDelay = calibration_delay;
+}
+
+/*
+ * Get the current Stop pulse in microseconds
+ *
+ */
+uint32_t getStopPulse(void)
+{
+	return stopPulse;
+}
+
+/*
+ * Set the current Stop pulse in microseconds
+ *
+ */
+void setStopPulse(uint32_t stop_pulse)
+{
+	stopPulse = stop_pulse;
 }
